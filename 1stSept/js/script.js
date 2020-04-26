0f4b476d60a7537c207219236e5b0f21061f6025
Alexandr Pashkevych <o632681383@gmail.com>
2020-04-26 21:37:18 +0300

    Done with yoga timer plus created 1september2020 timer for fun

--- 1stSept/js/script.js
+++ 1stSept/js/script.js
@@ -1,53 +1,22 @@
 window.addEventListener('DOMContentLoaded', function(){
 
-    'use strict'
+'use strict'
 
-    let tab = document.querySelectorAll('.info-header-tab'),
-        info = document.querySelector('.info-header'),
-        tabContant = document.querySelectorAll('.info-tabcontent');
-
-    function hideTabContant(a){
-        for(let i = a; i<tabContant.length; i++){
-            tabContant[i].classList.remove('show');
-            tabContant[i].classList.add('hide');
-        }
-    }
-
-    hideTabContant(1);
-
-    function showTabContant(b){
-        if(tabContant[b].classList.contains('hide')){
-            tabContant[b].classList.remove('hide');
-            tabContant[b].classList.add('show');
-        }
-    }
-
-    info.addEventListener('click', function(event){
-        let target = event.target;
-
-        if(target && target.classList.contains('info-header-tab')){
-            for(let i=0; i<tab.length; i++){
-                if(target === tab[i]){
-                    hideTabContant(0);
-                    showTabContant(i);
-                }
-            }
-        }
-    });
-
-    let deadline = '2020-08-01';
+let deadline = '2020-09-01';
 
     function getTimeRemaining (endTime){
         let timeDifference = Date.parse(deadline) - Date.parse(new Date()),
             seconds = Math.floor(timeDifference / 1000 % 60),
             mins = Math.floor(timeDifference/1000 / 60 %60),
-            hours = Math.floor(timeDifference / (1000 * 60 * 60));
+            hours = Math.floor(timeDifference / 1000 /60 / 60 % 24),
+            days = Math.floor(timeDifference/(1000*60*60*24));
 
             return{
                 'total': timeDifference,
                 'hours': hours,
                 'minutes': mins,
-                'seckonds': seconds
+                'seckonds': seconds,
+                'days': days
             };
             
     }
@@ -57,12 +26,20 @@ window.addEventListener('DOMContentLoaded', function(){
             hours = timer.querySelector('.hours'),
             minutes = timer.querySelector('.minutes'),
             seconds = timer.querySelector('.seconds'),
-              timeInterval = setInterval(updateClock, 1000);
+            days = timer.querySelector('.days'),
+            timeInterval = setInterval(updateClock, 1000);
+              
         
 
         function updateClock(){
+            
             let t = getTimeRemaining(endTime);
+            days.textContent = t.days;
+            if(t.hours >9){
             hours.textContent = + t.hours;
+            } else {
+                minutes.textContent = "0" + t.hours;
+            }
             if (t.minutes > 9){
             minutes.textContent = t.minutes;
             } else {
