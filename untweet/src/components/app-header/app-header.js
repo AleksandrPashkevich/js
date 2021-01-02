87a07db1af807b574669af92ae0980116397c713
Oleksandr Pashkevych <o632681383@gmail.com>
2021-01-02 10:54:58 +0200

    added deleting statuses (with problems)

--- untweet/src/components/app-header/app-header.js
+++ untweet/src/components/app-header/app-header.js
@@ -1,14 +1,36 @@
 import React from'react';
-import './app-header.css';
+
+import styled from 'styled-components';
+
+const Header = styled.div`
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-between;
+    h1 {
+        font-size:26px;
+        color: ${props => props.colored ? 'red' : 'black'};
+        :hover {
+            color: blue;
+        }
+    }
+
+    h2 {
+        font-size: 1.2rem;
+        color: grey;
+        :hover {
+            color: dimgrey;
+        }
+    }
+`
 
 const AppHeader = () => {
+    const numberOfLines = 6;
+    const liked = 2;
     return(
-        <div className="app-header d-flex">
+        <Header>
             <h1> Volgant Bobrilov</h1>
-            <h2>5 записей, из них понравилось 0</h2>
-
-
-        </div>
+            <h2> {numberOfLines} записей, из них понравилось {liked}</h2>
+        </Header>
     )
 }
 
--- untweet/src/components/app/app.js
+++ untweet/src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './app.css';
 import styled from 'styled-components';
@@ -10,33 +10,63 @@ import PostList from '../post-list';
 import PostAddForm from '../post-add-form';
 
 
-const AppBlock = styled.div `
+const AppBlock = styled.div`
     margin: 0 auto;
     max-width: 800px;`
-     
 
-    
 
-const App = () => {
 
-    const data = [
-        {label: "Going to lern React", important: false, id:1},
-        {label: "That is good", important: true, id: 2},
-        {label: "still learning....", important: false, id: 3},
-        {label: "i need a breake....", important: true, id: 5}
-    ];
 
-    return (
-        <AppBlock>
-            <AppHeader />
-            <div className="search-panel d-flex">
-                <SearchPannel />
-                <PostStatusFilter />
-            </div>
-            <PostList posts={data}/>
-            <PostAddForm/>
-        </AppBlock>
-    )
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                { label: "Going to lern React", important: false, id: 'erdn' },
+                { label: "That is good", important: true, id: 'esax' },
+                { label: "still learning....", important: false, id: 'pfbvu' },
+                { label: "i need a breake....", important: true, id: 'ujgfb' }
+            ]
+        };
+
+        this.deleteItem = this.deleteItem.bind(this);
+        this.addItem = this.addItem.bind(this);
+    }
+    deleteItem(id) {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+        
+            const before = data.slice(0, index);
+            const after = data.slice(index + 1);
+
+            const newArr = [...before, after];
+            
+            return {
+                data: newArr
+                
+            }
+        });
+    }
+
+    addItem(body){
+        console.log(body);
+    }
+    render() {
+
+        return (
+            <AppBlock>
+                <AppHeader />
+                <div className="search-panel d-flex">
+                    <SearchPannel />
+                    <PostStatusFilter />
+                </div>
+                <PostList posts={this.state.data}
+                    onDelete={this.deleteItem} />
+                <PostAddForm 
+                onAdd={this.addItem}/>
+            </AppBlock>
+        )
+    }
 }
 
-export default App;
+
